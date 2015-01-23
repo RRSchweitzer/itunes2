132e236e17505bbf2c7da2cc9337d413d44f18fe
Jason Turner <jasonalmaturner@gmail.com>
2015-01-23 11:10:48 -0700

    Added some notes about promises

--- js/itunesService.js
+++ js/itunesService.js
@@ -9,7 +9,25 @@ app.service('itunesService', function($http, $q){
   //Note that in the above line, artist is the parameter being passed in. 
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
-    //Code here
+
+// This function returns $http's promise to the controller, where this function was invoked. The .then in the controller is waiting
+// for http's promise to resolve or reject. It will run the first callback if http resolves the promise, which means the request was good.
+// It will run the second callback in the .then if the promise was rejected, or the http request was bad.
+this.getData = function(artist, type){
+	return $http({
+		method: 'JSONP',
+		url: 'https://itunes.apple.com/search?term=' + artist + '&media=' + type + '&callback=JSON_CALLBACK'
+	});
+};
+
+// This function returns the promise I built instead of the http request. I can control when my promise resolves or rejects.
+// This will tell the .then in the controller which callback to run. If I resolve my promise, then I'm telling the .then to run the first callback.
+// If I reject my promise, then I'm telling the .then in controller to run the second callback. I can also pass whatever information back to the .then
+// through my deferred.resolve or deferred.reject. Although I'm returning my own promise, and not the http promise, to the controller where this function was
+// invoked, I can still run off of http's promise. That's why I have a .then after my http request. The .then here is waiting for http's promise to resolve or reject.
+// So, if the http's promise resolves (the request was good and it worked), then I'm going to say my promise is good and I will resolve it. So, I run
+// deferred.resolve in the first .then callback. I'm essentially saying I'll keep my promise if http keeps its promise. If http rejects its promise,
+// then I'm going to reject my promise as well.
 this.getData2 = function(artist, type){
   var deferred = $q.defer();
     $http({
@@ -23,11 +41,48 @@ this.getData2 = function(artist, type){
   return deferred.promise;
 };
 
-this.getData = function(artist, type){
-	return $http({
-		method: 'JSONP',
-		url: 'https://itunes.apple.com/search?term=' + artist + '&media=' + type + '&callback=JSON_CALLBACK'
-	});
+
+// Just like above, I constructed my own promises. I'm calling .then on my http request. If the request is good, it is going to run the first callback.
+// If the request is good, that means I received usable data. I include all of my data manipulation logic within that first callback. I then resolve the 
+// promise I created. deferred.resolve tells the .then in the controller to run the first callback. I can also pass whatever data I want through my deferred.resolve or
+// deferred.reject. In my deferred.resolve below, I'm passing in my songArray. That array is my data after I've manipulated it to what I want it to look like.
+// Whatever I pass into deferred.resolve() will pass into the controller. Thus, by passing songArray in my deferred.resolve as I have below, it will spit out in .then
+// in the controller.
+// Example:
+// This is in my service where I created my promise.
+// deferred.resolve('it worked');
+// deferred.reject('poop')
+// This is my .then in the controller where this function was invoked.
+// .then(function(res){
+//   console.log(res);
+// }, function(err){
+//   console.log(err);
+// })
+// This will print 'it worked' in the console if my promise resolves, or 'poop' if it rejects.
+
+this.getData3 = function(artist, type){
+  var deferred = $q.defer();
+    $http({
+      method: 'JSONP',
+      url: 'https://itunes.apple.com/search?term=' + artist + '&media=' + type + '&callback=JSON_CALLBACK'
+    }).then(function(res){
+      var data = res.data.results;
+      var songArray = [];
+      for(var i = 0; i < data.length; i++){
+        var songObject = {
+          AlbumArt: data[i].artworkUrl100,
+          Artist: data[i].artistName,
+          Collection: data[i].collectionCensoredName,
+          Play: data[i].previewUrl,
+          Type: data[i].kind
+        };
+        songArray.push(obj);
+      }
+      deferred.resolve(songArray);
+    }, function(err){
+      deferred.reject(err);
+    });
+  return deferred.promise;
 };
 
 });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,9 +1,7 @@
 var app = angular.module('itunes');
 
 app.controller('mainCtrl', function($scope, itunesService){
-  //This is setting up the default behavior of our ng-grid. The important thing to note is
-  //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
-  //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
+
   $scope.gridOptions = { 
       data: 'songData',
       height: '110px',
@@ -19,54 +17,51 @@ app.controller('mainCtrl', function($scope, itunesService){
       filterOptions: $scope.filterBy
   };
 
-  //Our controller is what's going to connect our 'heavy lifting' itunesService with our view (index.html) so our user can see the results they get back from itunes.
-
-  //First inject itunesService into your controller.
-
-    //code here
 
 $scope.updateOrderInfo = function(){
   $scope.gridOptions.sortInfo
 }
-  //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
-  //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
-  //Also note that that method should be retuning a promise, so you could use .then in this function.
-    
-    //Code here
+
+
+// This getSongData function works with itunesService.getData or itunesService.getData2. The difference is which promise 
+// I'm running off of. 
 $scope.getSongData = function(){
+  // This just sets my search type to all if nothing was selected in the drop down box. I don't want to send a search with 'undefined' in my url
+  // to iTunes. This prevents that from happening. Read iTunes api documentation if you want to understand more.
   if(!$scope.search.type){
     $scope.search.type = 'all'
   };
-  itunesService.getData($scope.search.artist, $scope.search.type).then(function(res){
-    console.log(res.data.results);
-    parseInfo(res.data.results);
+  // Or itunesService.getData2($scope.search....)...
+  itunesService.getData($scope.search.artist, $scope.search.type).then(function(response){
+    // The data is contained in response. This function will only run if the promise is resolved.
+    console.log('the promise was resolved');
+    parseInfo(response.data.results);
     $scope.songData = parsedInfo;
     // $scope.artist = '';
     // console.log(parsedInfo);
     parsedInfo = [];
   }, function(err) {
+    // The data is contained in err. This callback/function will only run if the promise was rejected
+    console.log(err, 'The promise was rejected');
+  });
+};
+
+// This getSongData function works with itunesService.getData3. This moves my data manipulation from the controller into my service, but is functionally
+// the same as the function above. I've commented this function out, because I don't want both functions with the same name saved to the scope object.
+// I could rename the function below, but then I would have to change my ng-clicks and/or ng-changes in the view. I'm too lazy to do that. 
+// I included this for demonstration purposes.
+$scope.getSongData = function(){
+  if(!scope.search.type){
+    $scope.search.type = 'all'
+  };
+  itunesService.getData3($scope.search.artist, $scope.search.type).then(function(res){
+    $scope.songData = res;
+  }, function(err){
     console.log(err);
   });
 };
 
-  //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
-
-    //Code here
-
-
-  //If everything worked you should see a huge array of objects inside your console. That's great! But unfortunately that's not what ng-grid is expecting. What you need to do now
-  //is sort the data you got back to be an object in the following format.
-    /*
-      AlbumArt: "http://a3.mzstatic.com/us/r30/Features4/v4/22/be/30/22be305b-d988-4525-453c-7203af1dc5a3/dj.srlprmuo.100x100-75.jpg"
-      Artist: "Nelly"
-      Collection: "Nellyville"
-      CollectionPrice: 11.99
-      Play: "http://a423.phobos.apple.com/us/r1000/013/Music4/v4/4a/ab/7c/4aab7ce2-9a72-aa07-ac6b-2011b86b0042/mzaf_6553745548541009508.plus.aac.p.m4a"
-      Type: "song"
-  */
-  //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data.
-
-    //Code here
+// This is the same logic I have in itunesService.getSongData3. I still have it here because I'm using itunesService.getSongData
 var parsedInfo = [];
 
 var parseInfo = function(data) {
@@ -82,9 +77,6 @@ var parseInfo = function(data) {
   };
 };
 
-  //Once you have that final data array, you simply need to put it on the scope (or more specifically on the scope as songData). Once you do this ($scope.songData = myFinalArray) then ng-grid will see that and populate the page.
-
-    //Code here
 });
 
 
